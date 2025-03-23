cc3e9e53b2133bda73e28a5f8707b086f9fd3272
Jeff Hykin <jeff.hykin@gmail.com>
2025-03-23 16:13:33 -0500

    fix strength color

--- main/infinite_canvas.js
+++ main/infinite_canvas.js
@@ -71,10 +71,16 @@ function drawArcThroughPoints(ctx, x1, y1, x2, y2, x3, y3) {
 
 // Helper function to interpolate color based on edge strength
 function strengthToColor(strength) {
+    if (strength < -1) {
+        return strength = -1
+    }
+    if (strength > 1) {
+        return strength = 1
+    }
     if (strength <= 0) {
         return interpolateColor(red, blue, (strength + 1) ** 2 / 1)
     } else {
-        return interpolateColor(green, blue, 1 - Math.min(strength, 1))
+        return interpolateColor(green, blue, 1 - strength)
     }
 }
 
